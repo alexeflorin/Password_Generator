ac34b9b2748c81f0251e6cbf5949f18ea75c0748
alexeflorin <goremic@yahoo.com>
2023-04-19 18:31:20 -0400

    modified:script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,78 +1,70 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Arrayes with characters
+var includedLowerCaseLetter=["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+var includedUpperCaseLetter=["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+var includedNumbers=["0","1","2","3","4","5","6","7","8","9"];
+var includeSpecialCharacters=["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","/","^","_","`","{","|","}","~"];
+function getRandomNumber(min, max) {
+  var randomNumber=Math.random() // Random number between 0 and 0.99
+  var randomNumberUpToMax=randomNumber * max // Random number between 0 and max - 0.1
+  var randomNumberInRange= min + randomNumberUpToMax;
 
-
-function generatePassword(){
-  var generatePassword="";
-
-  var numberOfCharacters=prompt("How many characters you want the password to have?")
-  if (length =>8 && length <=128){
-    alert 
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-// do your things here
+  return Math.floor(randomNumberInRange); // Remove the decimal places
+}
+function getRandomValueFromArray(array) {
+    var randomArrayPosition=getRandomNumber(0,array.length);
+    return array[randomArrayPosition];
+}
+// Write password to the #password input
+function generatePassword() {
+ var password="";
 
 
 // 1- ask the user how many characters
-var numberOfCharacters=prompt(passwordLength);
-(passwordLength >= 8 && passwordLength <= 128)
+  var numberOfCharacters = (prompt("Choose the length of your password. This needs to be between 8 - 128"));
+  if (numberOfCharacters <=8 || numberOfCharacters >=128)
+  alert ("Please select a number between 8 - 128");
 
-
-// 2- ask if the user want to use special characters
-var includeSpecialCharacters=confirm("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
-
-// 3- ask if the user want to use upper case letters
-var includedUpperCaseLetter=confirm();
-// 4- ask if the user want to use lower case letters
-var includedLowerCaseLetter=confirm();
-// 5- ask if the user want to use numbers
-var includedNumbers=confirm();
-// 6- generate a password withth enumber of characters and the types of characters selected by the user and put it in the "password generated"
+  
+ var numberOfCharacters = (prompt("Choose the length of your password. This needs to be between 8 - 128"));
 
 
 
-
-
-
-
-  return generatePassword;
+// 2- ask if the user want to use lower case letters
+ var withLowerCaseLetter=confirm("Would you like to include lower case characters?");
+if (withLowerCaseLetter){
+  password+=getRandomValueFromArray(includedLowerCaseLetter);
 }
+// 3- ask if the user want to use upper case letters
+ var withUpperCaseLetter=confirm("Would you like to include upper case characters?");
+if (withUpperCaseLetter){
+  password+=getRandomValueFromArray(includedUpperCaseLetter);
+}
+// 4- ask if the user want to use numbers
+ var withNumbers=confirm("Would you like to include numbers?");
+if (withNumbers) {
+  password+=getRandomValueFromArray(includedNumbers);
+}
+// 5- ask if the user want to use special characters
+ var withSpecialCharacters=confirm("Would you like to include special characters?");
+if (withSpecialCharacters) {
+  password+=getRandomValueFromArray(includeSpecialCharacters);
+}
+
+
+
+// 6- generate a password with the number of characters and the types of characters selected by the user and put it in the "password generated"
+
+
+return password;
+  
 
 // return "password generated" is actually the password, not the word password generated...
 
 
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -81,10 +73,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+
 // The "writePassword" function is going to get whatever "var password" function returns.
 // It`s going to set the value resulted from "passwordText.value".
-
+}
 
 
 generateBtn.addEventListener("click", writePassword);
